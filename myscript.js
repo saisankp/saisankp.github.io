23e58420d0dddea871c74c27075a4dc6c68e5770
Prathamesh Sai <prathameshsaisankar@Gmail.com>
2020-07-02 16:21:45 +0100

    Site Version 2.0
    -Button for Multilingual text
    -Reduced number of pages on main menu.
    -More sleek layout for the about page.
    - Tamil School located under Events
    - Width of Menu adjusted for smaller devices.
    - Google forms width and height glitch fixed.

---
+++ myscript.js
@@ -0,0 +1,12 @@
+
+        function change() // no ';' here
+        {
+            var elem = document.getElementById("myButton1");
+            if (elem.value=="Tamil") elem.value = "English";
+            else elem.value = "Tamil";
+        
+        }
+        
+        
+        
+        
