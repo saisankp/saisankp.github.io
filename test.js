39d1e554370e09f2121ffca2d6602d3a8c909bf9
Prathamesh Sai <prathameshsaisankar@Gmail.com>
2020-07-06 20:18:44 +0100

    Changed Media page.

--- test.js
+++ test.js
@@ -1,3 +1,22 @@
+// MDB Lightbox Init
+$(function () {
+  $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+  });
+
+$(function() {
+  var selectedClass = "";
+  $(".filter").click(function(){
+  selectedClass = $(this).attr("data-rel");
+  $("#gallery").fadeTo(100, 0.1);
+  $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+  setTimeout(function() {
+  $("."+selectedClass).fadeIn().addClass('animation');
+  $("#gallery").fadeTo(300, 1);
+  }, 300);
+  });
+  });
+
+
 loadCSS = function(href) {
 
     var cssLink = $("<link>");
